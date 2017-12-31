345c1a19dc42b7d4f291a961a54427b5eb0cc724
BlissfulSomeone <33756858+BlissfulSomeone@users.noreply.github.com>
2017-12-31 16:36:25 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,13 @@ client.on('ready', () => {
 client.on('message', message => {
   if (message.author.bot) return;
   if (message.content.indexOf('!') !== 0) return;
-  message.channel.send('Hey!');
+  const args = message.content.slice(1).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  if (command == "ping")
+  {
+    const m = await message.channel.send("Ping?");
+    m.edit('Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms');
+  }
 });
 
 client.login(process.env.BOT_TOKEN);
