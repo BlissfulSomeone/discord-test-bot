f78c2dff4d7f597dd705f8a39bc9bd3531997f5d
BlissfulSomeone <33756858+BlissfulSomeone@users.noreply.github.com>
2017-12-31 15:36:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,21 +7,22 @@ client.on('ready', () => {
 });
 
 client.on('message', function (user, userID, channelID, message, evt) {
-  if (message.substring(0, 1) == '!')
-  {
-    var args = message.substring(1).split(' ');
-    var cmd = args[0];
-    args = args.splice(1);
-    switch (cmd)
-    {
-      case 'fetch':
-        client.sendMessage({to: channelID, message: '*fetches*'});
-        break;
-      case 'role':
-        let myRole = message.guild.roles.find(userID, "Some Role");
-        break;
-    }
-  }
+  //if (message.substring(0, 1) == '!')
+  //{
+  //  var args = message.substring(1).split(' ');
+  //  var cmd = args[0];
+  //  args = args.splice(1);
+  //  switch (cmd)
+  //  {
+  //    case 'fetch':
+  //      client.sendMessage({to: channelID, message: '*fetches*'});
+  //      break;
+  //    case 'role':
+  //      let myRole = message.guild.roles.find(userID, "Some Role");
+  //      break;
+  //  }
+  //}
+  client.sendMessage({to: channelID, message: 'hey!'});
 });
 
 client.login(process.env.BOT_TOKEN);
